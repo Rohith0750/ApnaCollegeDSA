0bfed3f88a161a4510a6395c5ecc021f4539380a
Rohith S D <rohithsd0222@gmail.com>
2025-09-24 22:40:52 +0530

    Added binary search

--- 13 ARRAYS/Largest.java
+++ 13 ARRAYS/Largest.java
@@ -2,7 +2,7 @@ public class Largest{
 
      public static void getlargest(int arr[]){
         int largest  = Integer.MIN_VALUE;
-        int smallest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
         for(int i=0;i<arr.length;i++){
             if(largest <arr[i]){
                 largest=arr[i];
@@ -14,8 +14,11 @@ public class Largest{
             
 
         }
+        System.out.println("Largest: " + largest);
+        System.out.println("Smallest: " + smallest);
      }
 
     public static void main(String[] args) {
         int arr []= {20,50,10,60,450,30};
+        getlargest(arr);
     }}
